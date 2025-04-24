eb2f16deda7190373392269edcf8fdc33c85dfa4
Carmichael <Alexander.Carmichael@gov.bc.ca>
2025-04-24 12:37:22 -0700

    bc gov branding and fluent is imported

--- gcpe-MediaHub/gcpe-mediahub.client/src/App.tsx
+++ gcpe-MediaHub/gcpe-mediahub.client/src/App.tsx
@@ -3,6 +3,12 @@ import Home from '../src/pages/Home/index';
 import { useState, useEffect, useCallback } from 'react';
 import { initializeKeycloak } from './services/keycloak';
 import { createContext } from 'react';
+import Header from './components/header';
+import {
+    FluentProvider,
+    webLightTheme,
+    Button
+} from "@fluentui/react-components";
 
 const router = createBrowserRouter([
     {
@@ -14,6 +20,7 @@ const router = createBrowserRouter([
 export const AuthenticationContext = createContext('authentication');
 
 function App() {
+    /* authentication stuff */
     const [keycloak, setKeycloak] = useState(null);
     const [isAuthenticated, setIsAuthenticated] = useState(false);
 
@@ -27,13 +34,19 @@ function App() {
         initKeycloak();
     }, [initKeycloak]);
 
+    /*Fluent UI stuff */
+
     return (
         <>
+            <FluentProvider theme={webLightTheme}>
+            <Header/>
+                <Button appearance="primary">Hello Fluent UI React</Button>
             {isAuthenticated && (
                 <AuthenticationContext.Provider value={keycloak}>
                     <RouterProvider router={router} />
                 </AuthenticationContext.Provider>
             )}
+            </FluentProvider>
         </>
     );
 }
---
+++ gcpe-MediaHub/gcpe-mediahub.client/src/components/header.tsx
@@ -0,0 +1,13 @@
+
+function Header() {
+    return (
+        <header>
+
+                <div><a href="/"><img src="./src/assets/bc-gov-theme/dist/images/bcid-mstile-70x70.png" /></a></div>
+                <div id="header-title">I fear no evil, for I am fear incarnate.</div>
+
+        </header>
+    );
+}
+
+export default Header;
