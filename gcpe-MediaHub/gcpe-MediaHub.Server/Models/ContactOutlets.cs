af2fa23155eb549078e8867a0735767f92c2d6d2
Carmichael <Alexander.Carmichael@gov.bc.ca>
2025-05-13 09:46:18 -0700

    Added ContactOutlet model

---
+++ gcpe-MediaHub/gcpe-MediaHub.Server/Models/ContactOutlets.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace gcpe_MediaHub.Server.Models
+{
+    public class ContactOutlets
+    {
+        [Key] 
+        public int Id { get; set; }
+
+        [Required]
+        public int ContactId { get; set; }
+        [ForeignKey("ContactId")]
+        public virtual MediaContact? Contact { get; set; }
+
+        [Required]
+        public int OutletId { get; set; }
+        [ForeignKey("OutletId")]
+        public virtual MediaOutlet? Outlet { get; set; }
+
+        [Required]
+        public string? ContactEmail { get; set; }
+    }
+}
