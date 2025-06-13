4062d9651f5f05f71dd1bf7647822709da7de21d
AlessiaYChen <alessia.yanchen@gov.bc.ca>
2025-06-13 16:10:52 -0700

    update

--- gcpe-MediaHub/gcpe-mediahub.client/vite.config.ts
+++ gcpe-MediaHub/gcpe-mediahub.client/vite.config.ts
@@ -1,63 +1,29 @@
 import { fileURLToPath, URL } from 'node:url';
-
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import plugin from '@vitejs/plugin-react';
-import fs from 'fs';
-import path from 'path';
-import child_process from 'child_process';
-import { env } from 'process';
-
-const baseFolder =
-    env.APPDATA !== undefined && env.APPDATA !== ''
-        ? `${env.APPDATA}/ASP.NET/https`
-        : `${env.HOME}/.aspnet/https`;
-
-const certificateName = "gcpe-mediahub.client";
-const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
-const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
-
-if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
-    if (0 !== child_process.spawnSync('dotnet', [
-        'dev-certs',
-        'https',
-        '--export-path',
-        certFilePath,
-        '--format',
-        'Pem',
-        '--no-password',
-    ], { stdio: 'inherit', }).status) {
-        throw new Error("Could not create certificate.");
-    }
-}
-
-const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:5173';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    base: './',
-    plugins: [plugin()],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
-        }
-    },
-    server: {
-        proxy: {
-           // 'mediaContacts': `${env.ASPNETCORE_HTTPS_PORT}/MediaContacts`,
-            '^/weatherforecast': {
-                target,
-                secure: false
-            },
-            '^/mediacontacts': {
-                target,
-                secure: false
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd());
+    const apiUrl = env.VITE_API_URL;
+
+    return {
+        base: '/',
+        plugins: [plugin()],
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url))
             }
         },
-        port: 5173,
-        https: {
-            key: fs.readFileSync(keyFilePath),
-            cert: fs.readFileSync(certFilePath),
+        server: {
+            proxy: {
+                '/api': {
+                    target: apiUrl,
+                    changeOrigin: true,
+                    secure: false
+                }
+            },
+            port: 5173
         }
     }
-})
+});
