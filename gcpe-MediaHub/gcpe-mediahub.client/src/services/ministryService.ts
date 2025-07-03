14761ed699f4641c8ff8b8e8c160581129e449a8
AlessiaYChen <alessia.yanchen@gov.bc.ca>
2025-07-03 13:40:51 -0700

    fix ministry list

--- gcpe-MediaHub/gcpe-mediahub.client/src/services/ministryService.ts
+++ gcpe-MediaHub/gcpe-mediahub.client/src/services/ministryService.ts
@@ -3,15 +3,16 @@ import { Ministry } from '../api/generated-client/model';
 
 // Create an axios instance with auth headers
 const axiosInstance = axios.create({
-    baseURL: '/',
+    baseURL: import.meta.env.VITE_API_URL,
     headers: {
-        'Authorization': `Basic ${btoa(import.meta.env.VITE_API_BASIC_AUTH ?? '')}`
+        'Authorization': `Basic ${btoa(import.meta.env.VITE_API_BASIC_AUTH ?? '')}`,
+        'Content-Type': 'application/json'
     }
 });
 
 export const ministryService = {
     async getMinistries(): Promise<Ministry[]> {
-        const response = await axiosInstance.get<Ministry[]>('/api/Ministries');
+        const response = await axiosInstance.get<Ministry[]>('Ministries');
         return response.data;
     }
 };
