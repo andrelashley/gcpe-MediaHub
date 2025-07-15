20fafaf923780266961977a617f93f8b4064c999
AlessiaYChen <alessia.yanchen@gov.bc.ca>
2025-07-15 11:16:06 -0700

    update banner styles

--- gcpe-MediaHub/gcpe-mediahub.client/src/components/AlphaInfo.tsx
+++ gcpe-MediaHub/gcpe-mediahub.client/src/components/AlphaInfo.tsx
@@ -1,6 +1,6 @@
 
-import { Button } from "@fluentui/react-components";
-import { Info24Regular, Dismiss24Regular } from "@fluentui/react-icons";
+import { Button, Body1Stronger, Body1 } from "@fluentui/react-components";
+import { Info24Regular, Dismiss24Regular  } from "@fluentui/react-icons";
 import styles from './MediaHeader.module.css';
 import { useState } from "react";
 
@@ -29,10 +29,15 @@ const AlphaInfo = () => {
                     <Info24Regular />
                 </div>
                 <div className={styles.alphaInfoContent}>
-                    <h3>Alpha release notice</h3>
-                    <p className={styles.disclaimerText}>This version is to test user satisfaction and collect feedback. Features may change, and updates will come frequently during this phase.
-                        If you experience any issues or have feedback, please share them in the <a className={styles.alphaReportLink} href='https://teams.microsoft.com/l/channel/19%3A97e62e1a8b9947479036fba8727207f6%40thread.tacv2/Media%20Hub%202.0%20UX?groupId=58c5efde-591f-4653-823a-c26fd95123df&tenantId=6fdb5200-3d0d-4a8a-b036-d3685e359adc&ngc=true'>Media Hub 2.0 Channel</a>.
-                    </p>
+                    <div style={{ paddingBottom: '8px' }}>
+                        <Body1Stronger>Alpha release notice</Body1Stronger>
+                    </div>
+                    <div>
+                        <Body1>
+                            This version is to test user satisfaction and collect feedback. Features may change, and updates will come frequently during this phase.
+                            If you experience any issues or have feedback, please share them in the <a className={styles.alphaReportLink} href='https://teams.microsoft.com/l/channel/19%3A97e62e1a8b9947479036fba8727207f6%40thread.tacv2/Media%20Hub%202.0%20UX?groupId=58c5efde-591f-4653-823a-c26fd95123df&tenantId=6fdb5200-3d0d-4a8a-b036-d3685e359adc&ngc=true'>Media Hub 2.0 Channel</a>.
+                        </Body1>
+                    </div>
 
                 </div>
                 <div className={styles.alphaDisclaimerDismiss}>
